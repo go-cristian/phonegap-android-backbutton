caf3d1e66c52077a4d5651a893ece014867de21e
Cristian Fabian <cristianfgr@gmail.com>
2014-09-10 21:25:46 -0500

    final implementation

--- src/android/BackPlugin.java
+++ src/android/BackPlugin.java
@@ -4,58 +4,66 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import android.annotation.SuppressLint;
-import android.app.FragmentManager.OnBackStackChangedListener;
-import android.content.Intent;
-import android.widget.Toast;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
 
 public class BackPlugin extends CordovaPlugin {
 
-	public static final String ACTION_ADD_CALENDAR_ENTRY = "addCalendarEntry";
+    public static final String ACTION_ADD_CALLBACK = "addCallback";
+    public static final String ACTION_REMOVE_CALLBACK = "removeCallback";
+    private CallbackContext callbackContext;
 
-	private CallbackContext callbackContext;
+    @Override
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        webView.setOnKeyListener(new OnKeyListener() {
 
-	@SuppressLint("NewApi")
-	@Override
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		// TODO Auto-generated method stub
-		OnBackStackChangedListener onBackStackChangedListener = new OnBackStackChangedListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
 
-			@Override
-			public void onBackStackChanged() {
-				Toast.makeText(BackPlugin.this.cordova.getActivity(),
-						"back called", Toast.LENGTH_LONG).show();
-				if (callbackContext != null) {
-					callbackContext.success();
-				}
-			}
-		};
-		this.cordova.getActivity().getFragmentManager()
-				.addOnBackStackChangedListener(onBackStackChangedListener);
-		Toast.makeText(this.cordova.getActivity(), "Init", Toast.LENGTH_LONG)
-				.show();
-		super.initialize(cordova, webView);
-	}
+                if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0
+                        && event.getAction() == KeyEvent.ACTION_UP) {
+                    if (callbackContext != null) {
+                        cordova.getThreadPool().execute(new Runnable() {
+                            public void run() {
+                                // Main Code goes here
+                                PluginResult result = new PluginResult(PluginResult.Status.OK);
+                                result.setKeepCallback(true);
+                                callbackContext.sendPluginResult(result);
+                            }
+                        });
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 
-	@Override
-	public boolean execute(String action, JSONArray args,
-			CallbackContext callbackContext) throws JSONException {
-		try {
-			if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) {
-				this.callbackContext = callbackContext;
-				return true;
-			}
-			callbackContext.error("Invalid action");
-			return false;
-		} catch (Exception e) {
-			System.err.println("Exception: " + e.getMessage());
-			callbackContext.error(e.getMessage());
-			return false;
-		}
-	}
+    @Override
+    public boolean execute(String action, JSONArray args,
+            CallbackContext callbackContext) throws JSONException {
+        try {
+            if (ACTION_ADD_CALLBACK.equals(action)) {
+                this.callbackContext = callbackContext;
+                return true;
+            }
+            if (ACTION_REMOVE_CALLBACK.equals(action)) {
+                this.callbackContext = null;
+                return true;
+            }
+            callbackContext.error("Invalid action");
+            return false;
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(e.getMessage());
+            return false;
+        }
+    }
 
 }
--- www/backbutton.js
+++ www/backbutton.js
@@ -1,13 +1,21 @@
-var calendarPlugin = {
-    createEvent: function(successCallback, errorCallback) {
-        console.log('to Cal exec');
+var backPlugin = {
+    createCallback: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'BackPlugin', // mapped to our native Java class called "CalendarPlugin"
-            'addCalendarEntry', // with this action name
+            'addCallback', // with this action name
+            []
+        ); 
+     },
+     removeCallback: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'BackPlugin', // mapped to our native Java class called "CalendarPlugin"
+            'removeCallback', // with this action name
             []
         ); 
      }
 }
-module.exports = calendarPlugin;
+module.exports = backPlugin;
